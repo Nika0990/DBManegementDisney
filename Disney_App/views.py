0300bda65251e63220842477f9afa15aedcc67d9
Nika <veronika@campus.technion.ac.il>
2022-12-31 13:43:01 +0200

    First query

--- Disney_App/views.py
+++ Disney_App/views.py
@@ -27,12 +27,43 @@ def add_movie(request):
 
 
 def query_results(request):
-    sql1 = """
-                SELECT genre, count(*) as yearCount
-                From(   SELECT DISTINCT M1.genre, year(M1.releaseDate) as years
-                FROM Movies as M1, Movies as M2
-                WHERE M1.genre = M2.genre and year(M1.releaseDate) = year(M2.releaseDate) and (M1.movieTitle != M2.movieTitle)
-                GROUP BY M1.genre, year(M1.releaseDate)) as movieYear
-                group by genre"""
-    sql_res1 = Movies.objects.raw(sql1)
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT Years.genre, yearCount, MaxGrossMovie, MaxGrossMovieGross, MaxLenMovie
+            FROM
+                       ( SELECT Zero.genre, ISNULL(GenreCount.yearCount,0) as yearCount
+                        FROM    (Select genre, 0 as yearCount
+                                From Movies
+                                Where genre is not null
+                                group by genre ) as Zero
+                                LEFT OUTER JOIN
+                                                (SELECT genre, count(*) as yearCount
+                                                    From(   SELECT DISTINCT M1.genre, year(M1.releaseDate) as years
+                                                            FROM Movies as M1 LEFT OUTER JOIN Movies as M2
+                                                            ON M1.genre = M2.genre
+                                                            WHERE M1.genre = M2.genre and year(M1.releaseDate) = year(M2.releaseDate)
+                                                                                      and (M1.movieTitle != M2.movieTitle) and M1.genre is not null
+                                                            GROUP BY M1.genre, year(M1.releaseDate)) as movieYear
+                                                        group by genre) as GenreCount
+                        ON Zero.genre = GenreCount.genre) as Years
+            FULL OUTER JOIN
+                        (SELECT maxGross.genre, movieTitle as MaxGrossMovie, MaxGrossMovieGross
+                        FROM (Select genre, MAX(gross) as MaxGrossMovieGross
+                                From Movies
+                                Where genre is not null
+                                GROUP BY genre) as maxGross LEFT OUTER JOIN Movies
+                        ON Movies.genre = maxGross.genre
+                        WHERE maxGross.MaxGrossMovieGross = Movies.gross) as MovieGross
+            ON MovieGross.genre = Years.genre
+            FULL OUTER JOIN
+                        (SELECT maxLenMovie.genre, min(movieTitle) as MaxLenMovie
+                        FROM
+                                (SELECT genre, MAX(len(movieTitle)) as maxLen
+                                FROM Movies
+                                WHERE genre is not null
+                                GROUP BY genre) as maxLenMovie
+                        FULL OUTER JOIN Movies ON Movies.genre = maxLenMovie.genre
+                        WHERE len(Movies.movieTitle) = maxLenMovie.maxLen
+                        GROUP BY maxLenMovie.genre) lenMovie
+            ON lenMovie.genre = MovieGross.genre""")
+        sql_res1 = dictfetchall(cursor)
     return render(request, 'query_results.html', {'sql_res_1': sql_res1})
